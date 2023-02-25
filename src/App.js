61683a7805370e2082c1db09959263247cc077a3
chhatreshkhatri <84904668+Chhatreshkhatri@users.noreply.github.com>
2023-02-25 12:10:13 +0530

    canonical + desc

--- src/App.js
+++ src/App.js
@@ -6,17 +6,10 @@ import { Skills } from "./components/Skills";
 import { Projects } from "./components/Projects";
 import { Footer } from "./components/Footer";
 import React from "react";
-import { Helmet } from "react-helmet";
 
 function App() {
   return (
     <div className="App">
-      <Helmet>
-        <title>Portfolio | Chhatresh Khatri</title>
-        <meta name="keywords" content="Portfolio, Chhatresh, Khatri, Chhatresh Khatri, Web Developer, Web Development, Website, Code, reactjs, front end"/>
-        <meta name="description" content="Hi, I am Chhatresh Khatri B. Tech in CSE. Full stack Web-Developer and a self-motivated, initiative-taking, and an enthusiastic professional."/>
-        <link rel="icon" href="/favicon.ico" />
-      </Helmet>
       <NavBar />
       <Banner />
       <Skills />
